5153921a59450469c2ecbff0afaa7398d906d26d
Vladislav <wlladisla@gmail.com>
2024-11-01 10:12:09 +0300

    feat: add base query for membertype

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,14 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import {
+  graphql,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+} from 'graphql';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -15,9 +23,54 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      // return graphql();
+      return graphql({
+        schema,
+        source: req.body.query,
+        variableValues: req.body.variables,
+      });
     },
   });
+  const MemberType = new GraphQLObjectType({
+    name: 'MemberType',
+    fields: () => ({
+      id: { type: new GraphQLNonNull(GraphQLString) },
+      discount: { type: new GraphQLNonNull(GraphQLString) },
+      postsLimitPerMonth: { type: new GraphQLNonNull(GraphQLString) },
+    }),
+  });
+  const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+      name: 'RootQuery',
+      fields: {
+        memberTypes: {
+          type: new GraphQLList(MemberType),
+          async resolve() {
+            return await prisma.memberType.findMany();
+          },
+        },
+        // discount: {
+        //   type: new GraphQLNonNull(GraphQLString),
+        // },
+        // postsLimitPerMonth: {
+        //   type: new GraphQLNonNull(GraphQLString),
+        // },
+      },
+    }),
+  });
 };
+
+// const schema = new GraphQLSchema({
+//   query: new GraphQLObjectType({
+//     name: 'RootQuery',
+//     fields: {
+//       testString: {
+//         type: GraphQLString,
+//         resolve: async () => {
+//           return 'hello world';
+//         },
+//       },
+//     },
+//   }),
+// });
 
 export default plugin;
